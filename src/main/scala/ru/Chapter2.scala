05a56f66d229dca1a5b7faf4302b9f00ef79e10c
Dmitry <chunindi@bcs.ru>
2019-11-21 12:13:08 +0300

    add tests on chapter 2 exercises

--- src/main/scala/ru/Chapter2.scala
+++ src/main/scala/ru/Chapter2.scala
@@ -47,7 +47,6 @@ object Chapter2 {
   implementation that compiles. Write this implementation.
   def curry[A,B,C](f: (A, B) => C): A => (B => C)
   */
-  //  todo add assert
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
     a: A => (b: B) => f(a, b)
   }
@@ -59,7 +58,6 @@ object Chapter2 {
   associates to the right, A => (B => C) can be written as A => B => C.
   def uncurry[A,B,C](f: A => B => C): (A, B) => C
   */
-  //  todo add assert
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
     (a: A, b: B) => f(a)(b)
   }
@@ -70,7 +68,6 @@ object Chapter2 {
   Implement the higher-order function that composes two functions.
   def compose[A,B,C](f: B => C, g: A => B): A => C
   */
-  //todo assert
   def compose[A, B, C](f: B => C, g: A => B): A => C = {
     a: A => f(g(a))
   }
--- src/test/scala/ru/Chapter2Test.scala
+++ src/test/scala/ru/Chapter2Test.scala
@@ -9,18 +9,42 @@ class Chapter2Test extends Assertions {
   }
 
   @Test def fibbonachiBoundaryValueTest(): Unit = {
-    assert(1 == Chapter2.fib(1))
+    assert(0 == Chapter2.fib(1))
     assert(1 == Chapter2.fib(2))
   }
 
+  private val as: Array[Int] = Array(-1, 0, 6, 6, 8, 9)
   @Test def isSortedPositiveTest(): Unit = {
     val ascendingOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x <= y
-    assert(Chapter2.isSorted(Array(-1, 0, 6, 6, 8, 9), ascendingOrder))
+    assert(Chapter2.isSorted(as, ascendingOrder))
   }
 
   @Test def isSortedNegativeTest(): Unit = {
+    val descendingOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x >= y
+    assert(!Chapter2.isSorted(as, descendingOrder))
+  }
+
+  @Test def curriedIsSortedPositiveAndNegativeTest(): Unit = {
     val ascendingOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x <= y
-    assert(!Chapter2.isSorted(Array(1, -4, 6, 0, 8, 9), ascendingOrder))
+    val descendingOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x >= y
+    val as: Array[Int] = Array(-1, 0, 6, 6, 8, 9)
+    val partiallyAppliedIsSorted = Chapter2.curry((anyArray: Array[Int], anySortingPredicate: (Int, Int) => Boolean) => Chapter2.isSorted(anyArray, anySortingPredicate)).apply(as)
+    assert(partiallyAppliedIsSorted.apply(ascendingOrder))
+    assert(!partiallyAppliedIsSorted.apply(descendingOrder))
+  }
+
+  @Test def uncurriedCurriedIsSortedPositiveTest(): Unit = {
+    val ascendingOrder: (Int, Int) => Boolean = (x: Int, y: Int) => x <= y
+    val as: Array[Int] = Array(-1, 0, 6, 6, 8, 9)
+    val curriedIsSorted = Chapter2.curry((anyArray: Array[Int], anySortingPredicate: (Int, Int) => Boolean) => Chapter2.isSorted(anyArray, anySortingPredicate))
+    assert(Chapter2.uncurry(curriedIsSorted).apply(as, ascendingOrder))
+  }
+
+  @Test def composePositiveTest(): Unit = {
+    val doubleAbs = (a: Double) => Math.abs(a)
+    val doubleSin = (a: Double) => Math.sin(a)
+    val angle = 0.5
+    assert(Math.abs(Math.sin(angle)) == Chapter2.compose(doubleAbs, doubleSin)(angle))
   }
 }
 
